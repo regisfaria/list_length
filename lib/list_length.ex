59c21051995f0c44dc89a5d5d91efa04196adea2
regisfaria <rgisfaria@yahoo.com.br>
2021-12-20 21:46:52 -0300

    finished the challenge

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count_length(list, 0)
+
+  defp count_length([], length), do: length
+
+  defp count_length([_head | tail], length) do
+    length = length + 1
+
+    count_length(tail, length)
   end
 end
--- mix.exs
+++ mix.exs
@@ -20,8 +20,6 @@ defmodule ListLength.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      {:json_response, git: "https://github.com/joaopealves/json_response"}
-    ]
+    []
   end
 end
